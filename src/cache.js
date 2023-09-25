7eb0f9e9a404968aecf711c8b82e3ec50d791a00
OKleg <ol.krol@yandex.ru>
2023-09-25 23:35:11 +0300

    test & code 72.72%

--- src/cache.js
+++ src/cache.js
@@ -1,7 +1,7 @@
 class Cache{
     
     constructor(){
-        this = {}
+        this.cache = {}
         this.count = {}
         this.hist = {}
     }
@@ -11,19 +11,22 @@ class Cache{
         if (!(typeof key === "string")){ throw new TypeError('key type') }
         if (!(typeof value === "string")){ throw new TypeError('value type') }
         if (!(typeof count === "number")){ throw new TypeError('count type') }
-        this[key] = value;
+        this.hist[key] =[];
+        this.cache[key] = value;
         this.count[key] = count;
+        this.hist[key].push([key, this.cache[key], this.count[key]]);
     };
     getCount(key){
         return this.count[key];
     }
     getValue(key){
+        if (!this.count[key] || this.count[key] == 0 ) return null 
         this.count[key]--;
-        this.hist[key].push([key,value, this.count[key], new Date().toLocaleString()]);
-        return this[key];
+        this.hist[key].push([key, this.cache[key], this.count[key]]);
+        return this.cache[key];
     }
     log(){
-        return this.hist.toString();
+        return this.hist;
     }
 }
 export {Cache}
--- test/cache.test.js
+++ test/cache.test.js
@@ -1,12 +1,13 @@
 import {Cache} from "../src/cache";
-//1
+//1 Реализуйте класс для работы с кэшем, поддерживающим конечное число обращений.
+//3 Для каждого ключа известно число обращений, после которого кэш должен очиститься.
 test('1', () => {
     let cache = new Cache();
     cache.push("чайник","1000", 3);
     let countReq = cache.getCount("чайник")
     expect(countReq).toBe(3);
 });
-//2
+//2 Кэш хранит пары ключ-значение.
 test('2', () => {
     let cache = new Cache();
     cache.push("чайник","1000", 3);
@@ -14,7 +15,7 @@ test('2', () => {
     let countReq = cache.getCount("чайник")
     expect(value).toBe("1000");
 });
-//4
+//4 После каждого обращения нужно уменьшить число обращений на 1.
 test('4', () => {
     let cache = new Cache();
     cache.push("чайник","1000", 3);
@@ -22,33 +23,46 @@ test('4', () => {
     let countReq = cache.getCount("чайник")
     expect(countReq).toBe(2);
 });
-//5
+//5 Если значения по ключу нет или если число обращений равно 0, то кэш возвращает null
 test('5', () => {
     let cache = new Cache();
     cache.push("чайник","1000", 1);
-    cache.getValue("чайник")
-    let countReq = cache.getCount("чайник")
-    let value =  cache.getValue("чайник")
+    cache.getValue("чайник");
+    let countReq = cache.getCount("чайник");
+    let value =  cache.getValue("чайник");
     expect(value).toBe(null);
 });
 
-//6
+//5 Если значения по ключу нет, то кэш возвращает null
+test('5', () => {
+    let cache = new Cache();
+    cache.push("чайник","1000", 1);
+    cache.getValue("чайник");
+    let countReq = cache.getCount("чайник");
+    let value =  cache.getValue("корги");
+    expect(value).toBe(null);
+});
+
+//6 Если пара ключ-значение задана без указания числа обращений, то число обращений равно 1.
 test('6', () => {
     let cache = new Cache();
     cache.push("чайник","1000");
-    cache.getValue("чайник")
-    let countReq = cache.getCount("чайник")
-    let value =  cache.getValue("чайник")
-    expect(value).toBe(null);
+    let countReq = cache.getCount("чайник");
+    expect(countReq).toBe(1);
 });
 
-//7
-// test('it fails', () => {
-//     let cache = new Cache();
-//     cache.push("чайник","1000");
-//     cache.getValue("чайник")
-//     let countReq = cache.getCount("чайник")
-//     let value =  cache.getValue("чайник")
-//     expect(value).toBe(null);
-// });
+//7 Нужна возможность получить статистику работы кэша в виде списка обращений:
+//Ключ, Значение, Сколько осталось обращений.
+test('7', () => {
+    let cache = new Cache();
+    cache.push("чайник","1000",4);
+    cache.getValue("чайник");
+    let log = cache.log();
+    expect(log["чайник"][0].toString()).toBe("чайник,1000,4");
+    expect(log["чайник"][0][2]).toBe(4);
+    expect(log["чайник"][1][2]).toBe(3);
+
+});
+
+
 
